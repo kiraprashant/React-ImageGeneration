cb460932d1b94cb95845d65ccac0166091760e88
Prashant Nair <prashantnair@Apples-MacBook-Air.local>
2024-10-21 00:40:35 +0530

    first commit

---
+++ src/cmp/Home.jsx
@@ -0,0 +1,10 @@
+import React ,{useEffect,useState} from 'react'
+import axios from 'axios'
+
+function Home() {
+  return (
+    <div className='container'>Home</div>
+  )
+}
+
+export default Home
---
+++ src/cmp/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className='container'>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a className="navbar-brand" href="#">Hidden brand</a>
+    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/">Home<span className="sr-only">(current)</span></NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/generate">Generate Image</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/View">View</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/logout">Logout</NavLink>
+      </li>
+    </ul>
+
+  </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
